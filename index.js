7b268e5314aa3e5ae8d68ca845ec5781fbbbac7f
Jayklef <jerryarhawho@gmail.com>
2023-10-05 10:34:59 +0100

    Get single user api

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/api/users', (req, res) =>{
     res.json(users)
 })
 
+app.get('/api/users/:id', (req, res) => {
+    res.json(users.filter(user => user.id === parseInt(req.params.id)))
+});
+
 const port = process.env.port || 5000;
 
 app.listen(port, () => console.log(`server started on port ${port}`));
